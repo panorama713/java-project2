a63d775167a873dfadbbbdf4cce1cc250dfb2400
김영근 <yaiba713@gmail.com>
2023-04-20 10:50:38 +0900

    feat: 입력받을때 입력하라는 문자를 보여줌 - ScannerEx2, ScannerEx2Test

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week1/day4/ScannerEx2Test.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1.day4;
+
+public class ScannerEx2Test {
+    public static void main(String[] args) {
+        ScannerEx2 se = new ScannerEx2();
+        se.inputTwoNumbersAndPlus();
+    }
+}
