699f3c9a3e091d1b952a43023befbc8c6657ff8d
김영근 <yaiba713@gmail.com>
2023-04-20 11:34:35 +0900

    feat: printf 연습 - MultiplicationTable

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week1/day4/MultiplicationTable.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week1.day4;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // 2 * 1 = 2
+        // 2 * 2 = 4
+        System.out.println("2" + " * " + "1" + " = " + "2");
+        System.out.printf("%d * %d = %d\n", 2, 2, 4);
+        System.out.printf("%d * %d = %d\n", 2, 4, 2 * 4);
+    }
+}
