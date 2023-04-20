76dd05cd22868af4adca91c60a729ae619b38fcc
김영근 <yaiba713@gmail.com>
2023-04-20 09:41:32 +0900

    feat: 두 숫자를 입력받은 뒤 더하는 기능 추가 - ScannerStrExTest, ScannerStrExTest.java

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week1/ScannerStrEx.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week1;
+
+import java.util.Scanner;
+
+public class ScannerStrEx {
+    void readStringsAndPlus() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next() + sc.next());
+    }
+}
---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week1/ScannerStrExTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject2.week1;
+
+public class ScannerStrExTest {
+    public static void main(String[] args) {
+        ScannerStrEx se = new ScannerStrEx();
+        se.readStringsAndPlus();
+    }
+}
