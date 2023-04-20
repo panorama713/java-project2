6b61f89a0b00b53baf84df1768ace841bc562473
김영근 <yaiba713@gmail.com>
2023-04-20 10:04:15 +0900

    feat: 코드업 문제풀이

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/CodeUp/CodeUp1007.java
@@ -0,0 +1,7 @@
+package com.example.javaproject2.CodeUp;
+
+public class CodeUp1007 {
+    public static void main(String[] args) {
+        System.out.println("\"C:\\Download\\hello.cpp\"");
+    }
+}
