4a46c0936c345b224e322f336e5d242a6aca1165
김영근 <yaiba713@gmail.com>
2023-04-21 16:00:50 +0900

    feat: codeUp1030-1034 문제풀이

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/CodeUp/CodeUp1030.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.CodeUp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CodeUp1030 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+
+        System.out.println(num);
+    }
+}
---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/CodeUp/CodeUp1034.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.CodeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.parseInt(sc.nextLine(), 8);
+        System.out.printf("%d", num);
+    }
+}
