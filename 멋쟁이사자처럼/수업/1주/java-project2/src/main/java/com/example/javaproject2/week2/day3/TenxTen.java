94dd234bc69655a7f38aef129d11e05a8ff94ff2
김영근 <yaiba713@gmail.com>
2023-04-26 10:33:03 +0900

    feat: 2차원 배열 연습 TenxTen.java ThreeThree.java

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/day3/TenxTen.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.day3;
+
+import java.util.Arrays;
+
+public class TenxTen {
+    public static void main(String[] args) {
+        int[][] arr = new int[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Arrays.toString(arr[i]));
+        }
+    }
+}
