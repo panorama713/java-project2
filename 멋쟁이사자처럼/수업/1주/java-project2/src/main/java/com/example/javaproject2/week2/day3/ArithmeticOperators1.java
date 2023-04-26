899956d4430e8b2c4581f4c433f7733edd1ab6b0
김영근 <yaiba713@gmail.com>
2023-04-26 14:11:15 +0900

    feat: 사칙연산 연습 ArithmeticOperators1.java ArithmeticOperators2.java

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/day3/ArithmeticOperators1.java
@@ -0,0 +1,24 @@
+package com.example.javaproject2.week2.day3;
+
+/**
+ * packageName : com.example.javaproject2.week2.day3
+ * fileName : ArithemticOperators1
+ * author : gim-yeong-geun
+ * date : 2023/04/26
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/04/26         gim-yeong-geun          최초 생성
+ */
+public class ArithmeticOperators1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.printf("a + b = %d\n", a + b);
+        System.out.printf("a - b = %d\n", a - b);
+        System.out.printf("a * b = %d\n", a * b);
+        System.out.printf("a / b = %d\n", a / b);
+        System.out.printf("a %% b = %d\n", a % b);
+    }
+}
---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/day3/ArithmeticOperators2.java
@@ -0,0 +1,24 @@
+package com.example.javaproject2.week2.day3;
+
+/**
+ * packageName : com.example.javaproject2.week2.day3
+ * fileName : ArithmeticOperators2
+ * author : gim-yeong-geun
+ * date : 2023/04/26
+ * description :
+ * ===========================================================
+ * DATE            AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023/04/26         gim-yeong-geun          최초 생성
+ */
+public class ArithmeticOperators2 {
+    public static void main(String[] args) {
+        // 몫
+        int quote = 10 / 3;
+        // 나머지
+        int remainder = 10 % 3;
+
+        System.out.println("quote : " + quote);
+        System.out.println("remainder : " + remainder);
+    }
+}
