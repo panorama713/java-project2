073b73a15a503c07daf0eed9aeb9733670de707c
김영근 <yaiba713@gmail.com>
2023-04-27 09:03:36 +0900

    feat: 누적합 연습 Accumulate687.java StringAccumulate.java

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/day3/Accumulate687.java
@@ -0,0 +1,9 @@
+package com.example.javaproject2.week2.day3;
+
+public class Accumulate687 {
+    public static void main(String[] args) {
+        int num = 687;
+        int answer = 0;
+
+    }
+}
