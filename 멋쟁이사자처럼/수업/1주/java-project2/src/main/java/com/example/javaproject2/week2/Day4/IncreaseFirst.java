87a96e389e134e506fa3c84dab77d5e8ac982aa2
김영근 <yaiba713@gmail.com>
2023-04-27 10:17:04 +0900

    feat: 증감연산자 연습 DecreaseOperator.java IncreaseFirst.java IncreaseFirst2.java IncreaseOperator.java

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/Day4/IncreaseFirst.java
@@ -0,0 +1,13 @@
+package com.example.javaproject2.week2.Day4;
+
+public class IncreaseFirst {
+    public static void main(String[] args) {
+        int idx = 0;
+        int[] arr = {2, 1, 7, 9};
+        System.out.println(arr[idx]);
+        idx += 1;
+        System.out.println(arr[idx]);
+        idx += 1;
+        System.out.println(arr[idx]);
+    }
+}
---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/Day4/IncreaseOperator.java
@@ -0,0 +1,10 @@
+package com.example.javaproject2.week2.Day4;
+
+public class IncreaseOperator {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+    }
+}
