06c3cf41d682bc744a6dbdcf2d55de81903b55d9
김영근 <yaiba713@gmail.com>
2023-04-27 11:48:35 +0900

    feat: boolean 연습 AndLogicalOperatorEx.java BoilerControl.java ComparisonOperatorsEx.java ComparisonOperatorsEx2.java LogicalOperatorEx.java

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/day4/AndLogicalOperatorEx.java
@@ -0,0 +1,14 @@
+package com.example.javaproject2.week2.day4;
+
+public class AndLogicalOperatorEx {
+    public static void main(String[] args) {
+        boolean result = true && true;
+        System.out.println(result);
+        result = true && false;
+        System.out.println(result);
+        result = false && true;
+        System.out.println(result);
+        result = false && false;
+        System.out.println(result);
+    }
+}
---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/day4/ComparisonOperatorsEx.java
@@ -0,0 +1,11 @@
+package com.example.javaproject2.week2.day4;
+
+public class ComparisonOperatorsEx {
+    public static void main(String[] args) {
+        int iVal1 = 10;
+        int iVal2 = 20;
+        boolean result = iVal1 == iVal2;
+        System.out.println("iVal1과 iVal2가 같은지? " + result);
+        System.out.println("iVal1과 iVal2가 다른지? " + (iVal1 != iVal2));
+    }
+}
