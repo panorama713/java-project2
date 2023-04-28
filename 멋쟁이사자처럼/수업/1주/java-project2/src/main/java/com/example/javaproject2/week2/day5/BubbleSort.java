a6d2e8fd97373e95506628f10d5ee74305b2ec38
김영근 <yaiba713@gmail.com>
2023-04-28 11:18:21 +0900

    feat: 버블정렬 기능 구현 BubbleSort.java

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week2/day5/BubbleSort.java
@@ -0,0 +1,35 @@
+package com.example.javaproject2.week2.day5;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 7, 9, 5, 4, 3};
+
+        while (true) {
+            // 정렬 실행 순회
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i+ 1] = tmp;
+                }
+            }
+
+            // 오름차순 정렬이 되었는지 확인
+            int check = 0;
+            for (int i = 0; i < arr.length - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    break;
+                } else {
+                    check++;
+                }
+            }
+            if (check == arr.length - 1) {
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
