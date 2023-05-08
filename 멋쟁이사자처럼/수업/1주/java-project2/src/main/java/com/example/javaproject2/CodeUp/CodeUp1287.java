858262135c578a54b6755031e5f2c48bbe929dfc
김영근 <yaiba713@gmail.com>
2023-05-08 09:25:24 +0900

    feat: codeUp

---
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/CodeUp/CodeUp1287.java
@@ -0,0 +1,16 @@
+package com.example.javaproject2.CodeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1287 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for (int i = 1; i < 10; i++) {
+            for (int j = 0; j < i * num; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
