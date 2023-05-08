2ad14ff30278fd92328eb9c4a4ca02a2c4e14f34
김영근 <yaiba713@gmail.com>
2023-05-08 11:15:18 +0900

    refactor: 코드 간소화

--- 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week4/day1/ForEx1.java
+++ 멋쟁이사자처럼/수업/1주/java-project2/src/main/java/com/example/javaproject2/week4/day1/ForEx1.java
@@ -6,14 +6,11 @@ public class ForEx1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        for (int i = 1; i < num + 1; i++) {
-            for (int j = num; j > i; j--) {
+        for (int i = 0; i < num; i++) {
+            for (int j = num - 1; j > i; j--) {
                 System.out.printf(" ");
             }
-            for (int j = 0; j < i; j++) {
-                System.out.printf("*");
-            }
-            for (int j = 1; j < i; j++) {
+            for (int j = 0; j < (2 * i) + 1; j++) {
                 System.out.printf("*");
             }
             System.out.println();
